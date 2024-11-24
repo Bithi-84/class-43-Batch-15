0025d3fd536ea0fb57573320ee6b49700b4b9ac2
Bithi-84 <tehzeebbithi.2016@gmail.com>
2024-11-24 21:50:09 +0600

    Data read

---
+++ config.php
@@ -0,0 +1,13 @@
+
+<?php
+$connection = new mysqli('localhost','root','','school_management_system');
+
+
+//  if($connection ){
+//      echo "successfully connected";
+//  }
+//  else{
+//     echo "connection Failed";
+//  }
+?>
+
